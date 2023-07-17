e3f0d396208c6e83679aee9d8cae35c56a8bb14f
Javed Chauhan <chauhanjaved@gmail.com>
2023-07-17 09:16:03 +0530

    Connect to MongoDB and start express

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0"
+
+const connectToMongo = async () => {
+    try {       
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Successfully connected to MongoDB')        
+
+    } catch (error) {
+        console.error('Failed to connect to MongoDB:',error);
+    }
+}
+module.exports = connectToMongo;
