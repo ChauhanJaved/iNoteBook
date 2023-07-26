562c899b60054d6c6f1e4d891673eac5839b1054
Javed Chauhan <chauhanjaved@gmail.com>
2023-07-26 09:24:39 +0530

    Add JWT authentication and new Endpoint for user Login This commit introduces JSON Web Token (JWT) for secure authentication and session handling. JWT was added to provide a stateless, secure way to manage user sessions. This implementation helps to verify the identity of users and protect sensitive endpoints from unauthorized access.
    Changes in this commit include:
    - Addition of JWT sign and verify functions.
    - Middleware function added to protect secure routes.
    - Update to user login and registration process to generate tokens upon successful authentication/registration."

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,13 @@ import { body, validationResult } from "express-validator";
 const router = express.Router();
 import UserSchema from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = 'thisismysecretkey@@@';
 
 // Create a User using: POST "/api/auth/". Doesn't require Auth
 router.post(
-  "/",
+  "/createuser",
   [
     // Validation middleware using express-validator
     // Check if the 'name' field is not empty
@@ -64,13 +67,66 @@ router.post(
       .then((savedUser) => {
         console.log("User saved:", savedUser);
         // Return the saved user as a JSON response
-        return res.json(newUser);
+        const data = { user: { id: newUser.id } };
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        return res.json(authtoken);
       })
       .catch((error) => {
         console.error("Error saving user:", error);
         // Return an error response as a JSON response
         return res.status(500).json({ error: "Internal Server Error" });
       });
+  }
+);
+
+// Endpoint for user login: POST "/api/auth/login"
+router.post(
+  "/login",
+  [
+    // Validation middleware using express-validator
+    // Check if the 'email' field is a valid email address
+    body("email").isEmail().withMessage("Enter a valid email address"),
+
+    // Check if the 'password' field exists (not empty)
+    body("password").exists().withMessage("Password cannot be blank"),
+  ],
+  async (req, res) => {
+    // Check for validation errors
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      // If there are validation errors, return them as a JSON response
+      return res.status(400).json({ errors: result.array() });
+    }
+
+    // Extract the data from req.body that you want to use for login
+    const { email, password } = req.body;
+    try {
+      // Create a Mongoose model called 'UserModel' based on 'UserSchema'
+      const UserModel = mongoose.model("users", UserSchema);
+      const user = await UserModel.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+
+      // Compare the provided password with the hashed password in the database
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct password" });
+      }
+
+      // If login successful, create a JWT token and send it as a JSON response
+      const data = { user: { id: user.id } };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      return res.json(authtoken);
+    } catch (error) {
+      console.error(error.message);
+      // Return an error response as a JSON response
+      res.status(500).send("Internal Server Error");
+    }
   }
 );
 
